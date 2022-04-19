a05a4eabb46b157b1fa1363691d368b7624219f4
Samuel <ssoderbe@kth.se>
2022-04-19 09:42:53 +0200

    Comments and easter egg

--- python-integration-test/assets/integration-test.py
+++ python-integration-test/assets/integration-test.py
@@ -6,9 +6,12 @@ from testcontainers.core.docker_client import DockerClient
 
 def test_check_logs():
     with DockerCompose(".") as compose:
+        # Starting docker
         docker = DockerClient()
         compose.wait_for("http://%s:8080/wd/hub" % docker.host())
         stdout, stderr = compose.get_logs()
+
+        # Checks that the first line of index.html exists in the output (client should print it)
         f = open("./server/index.html")
         expects = f.readline()
         assert expects in stdout.decode()
